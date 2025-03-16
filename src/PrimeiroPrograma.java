31c15533a4ef7e0715b775f68b046bfb4de07de5
Walisson Santos <walissonsantost008@gmail.com>
2025-03-16 14:11:49 -0300

    Revert "Fiz meu primeiro programa em java"
    This reverts commit 0a34d926c93efc6987c5df6739e7891949b4d41c.

--- src/PrimeiroPrograma.java
+++ src/PrimeiroPrograma.java
@@ -1,7 +1,5 @@
 public class PrimeiroPrograma {
     public static void main(String[] args){
         System.out.println("Fala cachorro");
-        String nome = "walisson";
-        System.out.println(nome);
     }
 }
