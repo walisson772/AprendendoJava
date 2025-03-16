0a34d926c93efc6987c5df6739e7891949b4d41c
Walisson Santos <walissonsantost008@gmail.com>
2025-03-16 14:08:28 -0300

    Fiz meu primeiro programa em java

--- src/PrimeiroPrograma.java
+++ src/PrimeiroPrograma.java
@@ -1,5 +1,7 @@
 public class PrimeiroPrograma {
     public static void main(String[] args){
         System.out.println("Fala cachorro");
+        String nome = "walisson";
+        System.out.println(nome);
     }
 }
