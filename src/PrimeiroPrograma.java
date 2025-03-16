268cba5c3f2226bea05a28e2a5b67b3eed1c4b60
Walisson Santos <walissonsantost008@gmail.com>
2025-03-16 14:33:59 -0300

    Fiz meu primeiro programa em java

--- src/PrimeiroPrograma.java
+++ src/PrimeiroPrograma.java
@@ -1,5 +1,11 @@
 public class PrimeiroPrograma {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Fala cachorro");
+        int idade = 19;
+        if (idade < 18) {
+            System.out.println("Você é menor de idade");
+        }else {
+            System.out.println("Você é maior de idade");
+        }
     }
 }
