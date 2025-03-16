d4906d6b846296e4c35a6d679cb383e21992d0a5
Walisson Santos <walissonsantost008@gmail.com>
2025-03-16 17:57:46 -0300

    Fiz meu primeiro programa em java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "walisson";
+        DigaOla(nome);
+    }
+    public static void DigaOla(String nome) {
+        System.out.println(nome);
+    }
+}
