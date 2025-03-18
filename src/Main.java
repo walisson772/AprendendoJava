4259e9500c2620f92d9131f7ded4977946d98030
Walisson Santos <walissonsantost008@gmail.com>
2025-03-18 14:23:54 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        String nome = "walisson";
-        printHello(nome);
-    }
-    public static void printHello(String nome) {
-        System.out.println(nome);
+        System.out.println("linha 3");
+        System.out.println("linha 4");
+        System.out.println("linha 5");
+        System.out.println("linha 6");
+        System.out.println("linha 7");
     }
 }
