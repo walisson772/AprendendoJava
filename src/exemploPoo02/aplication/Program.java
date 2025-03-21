10faed1082498fca89fd1e1da8b15a1cdcec8078
Walisson Santos <walissonsantost008@gmail.com>
2025-03-21 15:58:33 -0300

    Poo com java

--- src/exemploPoo02/aplication/Program.java
+++ src/exemploPoo02/aplication/Program.java
@@ -2,15 +2,21 @@ package exemploPoo02.aplication;
 
 import java.util.Locale;
 import java.util.Scanner;
-import entities.Product;
+import exemploPoo02.entities.*;
 
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         
-        Product product = new Product;
- 
+        Product product = new Product();
+        System.out.print("Adicione um produto:");
+        System.out.print("Nome:");
+        product.name = sc.nextLine();
+        System.out.print("Preco:");
+        product.preco = sc.nextDouble();
+        System.out.print("Quantidade:");
+        product.quantidade = sc.nextInt();
         sc.close();
 
     }
