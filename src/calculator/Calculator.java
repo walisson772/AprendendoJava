9d89a20d932ec490232e5210b5a9ce1343e53423
Walisson Santos <walissonsantost008@gmail.com>
2025-03-22 13:23:07 -0300

    Fiz uma calculadora simples para praticar poo no java

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,4 +1,38 @@
 package calculator;
 
 public class Calculator {
+    public double num1;
+    public double num2;
+
+    public void opcoes() {
+        System.out.println("1) Somar");
+        System.out.println("2) Subtrair");
+        System.out.println("3) Dividir");
+        System.out.println("4) Multiplicar");
+        System.out.println("5) Sair");
+    }
+
+    public double soma() {
+        this.num1 = num1;
+        this.num2 = num2;
+        return this.num1 + this.num2;
+    }
+
+    public double subtrair() {
+        this.num1 = num1;
+        this.num2 = num2;
+        return this.num1 - this.num2;
+    }
+
+    public double dividir() {
+        this.num1 = num1;
+        this.num2 = num2;
+        return this.num1 / this.num2;
+    }
+
+    public double multiplicar() {
+        this.num1 = num1;
+        this.num2 = num2;
+        return this.num1 * this.num2;
+    }
 }
--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,4 +1,44 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int opcoes;
+        double result;
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            Calculator calcula = new Calculator();
+            System.out.println("Digite um numero:");
+            calcula.num1 = sc.nextDouble();
+            System.out.println("Digite outro numero:");
+            calcula.num2 = sc.nextDouble();
+
+            calcula.opcoes();
+            opcoes = sc.nextInt();
+            if (opcoes == 1){
+                result = calcula.soma();
+                System.out.println(result);
+            }
+            else if (opcoes == 2) {
+                result = calcula.subtrair();
+                System.out.println(result);
+            }
+            else if (opcoes == 3) {
+                result = calcula.dividir();
+                System.out.println(result);
+            }
+            else if (opcoes == 4) {
+                result = calcula.multiplicar();
+                System.out.println(result);
+            }
+            else if (opcoes == 5) {
+                break;
+            }
+            else {
+                System.out.println("Essa opção não existe");
+            }
+        }
+        sc.close();
+    }
 }
