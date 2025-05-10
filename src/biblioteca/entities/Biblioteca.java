88fa03820842efe59647c13a007e342545020766
Walisson Santos <walissonsantost008@gmail.com>
2025-05-10 16:42:21 -0300

    Novo metodo para listar os livros e o toString para formatar

--- src/biblioteca/entities/Biblioteca.java
+++ src/biblioteca/entities/Biblioteca.java
@@ -1,21 +1,22 @@
 package biblioteca.entities;
 
+import java.util.List;
+
 public class Biblioteca {
 
     private Integer id;
     private String titulo;
     private String autor;
     private Integer anoPublicacao;
-    private Boolean disponivel;
+    private Boolean disponivel = true;
 
     public Biblioteca(){}
 
-    private Biblioteca(Integer id, String titulo, String autor, Integer anoPublicacao, Boolean disponivel){
+    public Biblioteca(Integer id, String titulo, String autor, Integer anoPublicacao){
         this.id = id;
         this.titulo = titulo;
         this.autor = autor;
         this.anoPublicacao = anoPublicacao;
-        this.disponivel = disponivel;
     }
 
     public Integer getId(){
@@ -55,11 +56,25 @@ public class Biblioteca {
     }
 
     public void op(){
+        System.out.println("=====================================================");
         System.out.println("1 - Cadastrar livros");
         System.out.println("2 - Listar livros disponíveis");
         System.out.println("3 - Emprestar um livro");
         System.out.println("4 - Devolver um livro");
         System.out.println("5 - Sair");
+        System.out.println("=====================================================");
         System.out.print("O que você deseja? ");
+
     }
+
+    public void listarLivros(List livros){
+        for (Object livro : livros){
+            System.out.println(livro);
+        }
+    }
+
+    public String toString(){
+        return "Id: " + id + " Titulo: " + titulo + " autor: " + autor + " disponivel: " + disponivel;
+    }
+
 }
